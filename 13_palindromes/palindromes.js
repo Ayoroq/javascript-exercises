8c7965161abc2bee3cfa5e73e56bfe99c7f7f969
Ayo <Ayoroq@gmail.com>
2025-07-09 18:54:54 -0400

    Adding the solutions to the exercises for question 12 to 16

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+// function to check if a string is a palindrome or not
+const palindromes = function (string) {
+    // get the palindrome of the string and remove spaces and unwanted characters
+  const palindrome = string.split("").reverse().join("").replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  // get the string and remove spaces and unwanted characters
+  string = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  // check if the palindrome is the same as the string
+  return palindrome === string
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    let oldestPerson = people[0];
+    for (person in people){
+        if(!people[person].yearOfDeath){
+            people[person].age = currentYear - people[person].yearOfBirth;
+        } else {
+            people[person].age = people[person].yearOfDeath - people[person].yearOfBirth;
+        }
+        if(people[person].age > oldestPerson.age){
+            oldestPerson = people[person];
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
